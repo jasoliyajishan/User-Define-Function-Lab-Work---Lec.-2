2b8696d0c61ddf8f8bf8778a3559dc8e4625ed51
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-06-06 00:27:22 -0700

    Add files via upload

---
+++ m4139-1.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+calc(int n)
+{
+
+      int i,a,b;
+      do{
+      	if(n==6)
+      	{
+      		printf("exit....\n");
+		  }
+		  else if(n>6)
+		  {
+		  	printf("plz proper choice enter.\n");
+		  }
+		  else if(n<6)
+		  {
+		  	switch(n)
+		  	{
+		  		case 1:
+		  			printf("enter value a : ");
+		  			scanf("%d",&a);
+		  			printf("enter value b : ");
+		  			scanf("%d",&b);
+		  			printf("adition of %d+%d=%d\n",a,b,a+b);
+		  			break;
+		  		case 2:
+		  			printf("enter value a : ");
+		  			scanf("%d",&a);
+		  			printf("enter value b : ");
+		  			scanf("%d",&b);
+		  			printf("substraction of %d-%d=%d\n",a,b,a-b);
+		  			break;
+		  		case 3:
+		  			printf("enter value a : ");
+		  			scanf("%d",&a);
+		  			printf("enter value b : ");
+		  			scanf("%d",&b);
+		  			printf("multiplication of %d*%d=%d\n",a,b,a*b);
+		  			break;
+		  		case 4:
+		  			printf("enter value a : ");
+		  			scanf("%d",&a);
+		  			printf("enter value b : ");
+		  			scanf("%d",&b);
+		  			printf("division of %d/%d=%d\n",a,b,a/b);
+		  			break;
+		  		case 5:
+		  			printf("enter value a : ");
+		  			scanf("%d",&a);
+		  			printf("enter value b : ");
+		  			scanf("%d",&b);
+		  			
+		  			printf("moduls of a&b=%d\n",a%b);
+		  			break;
+		  		}
+			  }
+		  }while(n!=6);
+	  
+}
+
+main()
+{
+	int n;
+	printf("press 1 to addtion\n");
+	printf("press 2 to substration\n");
+	printf("press 3 to multiplication\n");
+	printf("press 4 to division\n");
+	printf("press 5 to moduls\n");
+	printf("press 6 to exit\n");
+	printf("enter your choice : \n");
+	scanf("%d",&n);
+	calc(n);
+}
